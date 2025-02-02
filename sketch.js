2db486432cf094e78e493a9b3f4e50b4e1547d72
AmyLamey123 <163696228+AmyLamey123@users.noreply.github.com>
2025-02-01 18:13:52 -0600

    fix
    i did an oopsie

--- sketch.js
+++ sketch.js
@@ -11,13 +11,14 @@ let dropdown;
 let combined
 let music;
 let Songs;
-let amp;
-let vol;
+let fft;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(512, 512);
+  angleMode(DEGREES);
+  colorMode(HSB);
   
-  amp= new p5.Amplitude()
+  fft= new p5.FFT(0.6,256);
   
   button=createButton('λ');
   button.style('font-size', '30px');
@@ -26,7 +27,7 @@ function setup() {
   button.mousePressed(PressedTheButton);
   
   slider=createSlider(0, 1, 1, 0.01);
-  slider.position(width/2-slider.width, height/2+slider.height*8);
+  slider.position(width/2-slider.width, height/2+slider.height*10);
   slider.style('width','300px');
   slider.changed(()=> {
     music.setVolume(slider.value());
@@ -37,8 +38,8 @@ function setup() {
   dropdown.option("We've Got Hostiles");
   dropdown.option('Surface Tension 1');
   dropdown.option('Hazardous Environments');
-  dropdown.option('The Only Thing They Fear Is You')
-  dropdown.option('Bfg Division')
+  dropdown.option('The Only Thing They Fear Is You');
+  dropdown.option('Bfg Division');
   dropdown.option('Who Can It Be Now');
   dropdown.option('I Am Alone');
   dropdown.changed(songChanged)
@@ -47,37 +48,47 @@ function setup() {
   var combined=join(Songs,'.')
   music=loadSound(combined);
   //noStroke();
+  
+  w=width/64;
 }
 
 function songChanged(){
   Songs=[dropdown.selected(), 'mp3']
-  var combined=join(Songs,'.')
+  var combined=join(Songs,'.');
   music=loadSound(combined, playSong);
   
 }
 
 function playSong() {
-  music.stop()
-  music.play()
+  music.stop();
+  music.play();
 
 }
 
 function PressedTheButton(){
   if (music.isPlaying()){
-  music.stop()
+  music.stop();
   }
   else{
-  music.play()
+  music.play();
   }
 }
 
 function draw() {
   background('#292929');
-  vol=amp.getLevel()
-  for(i=0;i<400;i+=25){
-    for(j=0;j<400;j+=25){
-      fill(vol*1000,vol*200,j/2)
-      ellipse(i+random(vol*30),j+random(vol*30),vol*250)
-    }
+  var spectrum=fft.analyze();
+  console.log(spectrum);
+  //noStroke(); 
+  translate (width/2, height/2)
+  beginShape()
+  for (var i = 0; i<spectrum.length; i++){
+    stroke(i*10000,255,255)
+    var angle = map(i,0,spectrum.length,0,360)
+    var amp = spectrum[i];
+    var r = map(amp,0,256,100,200);
+    var x = r*cos(angle);
+    var y = r*sin(angle);
+    line(0,0,x,y);
   }
+  endShape()
 }
